2a45267e34095c7bcd9a05fd2b4362e1f01351d1
DESKTOP-S2TE7Q6\123 <thanhtung.nguyen211192@gmail.com>
2023-08-17 16:31:36 +0700

    no message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 12;
 
+    private bool canMove = true;
+
     private bool canDoubleJump = true;
+    private bool canWallSlide;
+    private bool isWallSliding;
 
     private bool facingRight = true;
     private float movingImput;
@@ -22,6 +26,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float groundCheckRadius;
     [SerializeField] private LayerMask whatIsGround;
     private bool isGrounded;
+
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallCheckDistance;
+    private bool isWallDetected;
 
 
     // Start is called before the first frame update
@@ -34,17 +42,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movingImput = Input.GetAxisRaw("Horizontal");
+        CheckInput ();
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            JumpButton();
-        }
 
-        if(isGrounded)
-        {
-            canDoubleJump = true;
-        }
 
         CollisionCheck();
         FlipController();
@@ -53,8 +53,38 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(movingImput * speed, rb.velocity.y);
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+        if (isWallDetected && canWallSlide)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y * 0.1f);
+        }
+        else
+        {
+            isWallSliding = false;
+            Move();
+        }
+    }
 
+    private void CheckInput()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpButton();
+        }
+        if (canMove)
+            movingImput = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    private void Move()
+    {
+        if (canMove)
+            rb.velocity = new Vector2(movingImput * speed, rb.velocity.y);
     }
 
     private void JumpButton()
@@ -83,9 +113,20 @@ public class PlayerController : MonoBehaviour
         anim.SetFloat("yVelocity", rb.velocity.y);
         anim.SetBool("isGrounded", isGrounded);
         anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isWallSliding", isWallSliding);
     }
     private void FlipController()
     {
+
+        if (isGrounded && isWallDetected)
+        {
+            if (facingRight && movingImput < 0)
+                Flip();
+            if (!facingRight && movingImput > 0)
+                Flip();
+        }
+
+
         if (rb.velocity.x > 0 && !facingRight)
             Flip();
         else if (rb.velocity.x < 0 && facingRight)
@@ -100,11 +141,17 @@ public class PlayerController : MonoBehaviour
     private void CollisionCheck()
     {
         isGrounded = Physics2D.OverlapCircle(groudCheck.position, groundCheckRadius, whatIsGround);
+        isWallDetected = Physics2D.Raycast(wallCheck.position, Vector2.right, wallCheckDistance, whatIsGround);
+
+        if (!isGrounded && rb.velocity.y < 0)
+            canWallSlide = true;
+
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(groudCheck.position, groundCheckRadius);
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
     }
 
 }
